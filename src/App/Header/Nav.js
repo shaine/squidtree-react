0edecafab341079e4dc097e9f2cefeb7d2f2dc69
Shaine Hatch <shaine@squidtree.com>
2020-09-23 10:42:32 -0600

    chore(content): Update homepage links

--- src/App/Header/Nav.js
+++ src/App/Header/Nav.js
@@ -12,14 +12,10 @@ function mapStateToProps(state) {
 
 function Nav({ radarEnabled }) {
     return (<ul className="nav">
-        {/* <li><LoginLink /></li> */}
-        {radarEnabled && <li><a href="/radar">Tech Radar</a></li>}
         <li><a href="/resume.pdf">Résumé</a></li>
         <li><a href="https://github.com/shaine">GitHub</a></li>
         <li><a href="https://github.com/pulls?q=is%3Apr+author%3Ashaine+is%3Aclosed+is%3Apublic">OSS Contributions</a></li>
-        <li><a href="https://twitter.com/ShaineHatch">T̅ͮ̄̂͌̏͂witter</a></li>
         <li><a href="https://www.linkedin.com/in/shaine/">LinkedIn</a></li>
-        <li><a href="/public/archer.jpg">Cat Photo</a></li>
     </ul>);
 }
 
